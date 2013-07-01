fcd6b7e04d17ec1a7f731c8294ecadc21dcc7438
jooskim <jsk9260@gmail.com>
2013-07-01 13:56:23 -0400

    Back button for multi-level pages

--- sites/all/themes/cfe/js/script.js
+++ sites/all/themes/cfe/js/script.js
@@ -7,14 +7,14 @@
 !function(a){var b=function(a,b){this.init("tooltip",a,b)};b.prototype={constructor:b,init:function(b,c,d){var e,f,g,h,i;this.type=b,this.$element=a(c),this.options=this.getOptions(d),this.enabled=!0,g=this.options.trigger.split(" ");for(i=g.length;i--;)h=g[i],h=="click"?this.$element.on("click."+this.type,this.options.selector,a.proxy(this.toggle,this)):h!="manual"&&(e=h=="hover"?"mouseenter":"focus",f=h=="hover"?"mouseleave":"blur",this.$element.on(e+"."+this.type,this.options.selector,a.proxy(this.enter,this)),this.$element.on(f+"."+this.type,this.options.selector,a.proxy(this.leave,this)));this.options.selector?this._options=a.extend({},this.options,{trigger:"manual",selector:""}):this.fixTitle()},getOptions:function(b){return b=a.extend({},a.fn[this.type].defaults,this.$element.data(),b),b.delay&&typeof b.delay=="number"&&(b.delay={show:b.delay,hide:b.delay}),b},enter:function(b){var c=a.fn[this.type].defaults,d={},e;this._options&&a.each(this._options,function(a,b){c[a]!=b&&(d[a]=b)},this),e=a(b.currentTarget)[this.type](d).data(this.type);if(!e.options.delay||!e.options.delay.show)return e.show();clearTimeout(this.timeout),e.hoverState="in",this.timeout=setTimeout(function(){e.hoverState=="in"&&e.show()},e.options.delay.show)},leave:function(b){var c=a(b.currentTarget)[this.type](this._options).data(this.type);this.timeout&&clearTimeout(this.timeout);if(!c.options.delay||!c.options.delay.hide)return c.hide();c.hoverState="out",this.timeout=setTimeout(function(){c.hoverState=="out"&&c.hide()},c.options.delay.hide)},show:function(){var b,c,d,e,f,g,h=a.Event("show");if(this.hasContent()&&this.enabled){this.$element.trigger(h);if(h.isDefaultPrevented())return;b=this.tip(),this.setContent(),this.options.animation&&b.addClass("fade"),f=typeof this.options.placement=="function"?this.options.placement.call(this,b[0],this.$element[0]):this.options.placement,b.detach().css({top:0,left:0,display:"block"}),this.options.container?b.appendTo(this.options.container):b.insertAfter(this.$element),c=this.getPosition(),d=b[0].offsetWidth,e=b[0].offsetHeight;switch(f){case"bottom":g={top:c.top+c.height,left:c.left+c.width/2-d/2};break;case"top":g={top:c.top-e,left:c.left+c.width/2-d/2};break;case"left":g={top:c.top+c.height/2-e/2,left:c.left-d};break;case"right":g={top:c.top+c.height/2-e/2,left:c.left+c.width}}this.applyPlacement(g,f),this.$element.trigger("shown")}},applyPlacement:function(a,b){var c=this.tip(),d=c[0].offsetWidth,e=c[0].offsetHeight,f,g,h,i;c.offset(a).addClass(b).addClass("in"),f=c[0].offsetWidth,g=c[0].offsetHeight,b=="top"&&g!=e&&(a.top=a.top+e-g,i=!0),b=="bottom"||b=="top"?(h=0,a.left<0&&(h=a.left*-2,a.left=0,c.offset(a),f=c[0].offsetWidth,g=c[0].offsetHeight),this.replaceArrow(h-d+f,f,"left")):this.replaceArrow(g-e,g,"top"),i&&c.offset(a)},replaceArrow:function(a,b,c){this.arrow().css(c,a?50*(1-a/b)+"%":"")},setContent:function(){var a=this.tip(),b=this.getTitle();a.find(".tooltip-inner")[this.options.html?"html":"text"](b),a.removeClass("fade in top bottom left right")},hide:function(){function e(){var b=setTimeout(function(){c.off(a.support.transition.end).detach()},500);c.one(a.support.transition.end,function(){clearTimeout(b),c.detach()})}var b=this,c=this.tip(),d=a.Event("hide");this.$element.trigger(d);if(d.isDefaultPrevented())return;return c.removeClass("in"),a.support.transition&&this.$tip.hasClass("fade")?e():c.detach(),this.$element.trigger("hidden"),this},fixTitle:function(){var a=this.$element;(a.attr("title")||typeof a.attr("data-original-title")!="string")&&a.attr("data-original-title",a.attr("title")||"").attr("title","")},hasContent:function(){return this.getTitle()},getPosition:function(){var b=this.$element[0];return a.extend({},typeof b.getBoundingClientRect=="function"?b.getBoundingClientRect():{width:b.offsetWidth,height:b.offsetHeight},this.$element.offset())},getTitle:function(){var a,b=this.$element,c=this.options;return a=b.attr("data-original-title")||(typeof c.title=="function"?c.title.call(b[0]):c.title),a},tip:function(){return this.$tip=this.$tip||a(this.options.template)},arrow:function(){return this.$arrow=this.$arrow||this.tip().find(".tooltip-arrow")},validate:function(){this.$element[0].parentNode||(this.hide(),this.$element=null,this.options=null)},enable:function(){this.enabled=!0},disable:function(){this.enabled=!1},toggleEnabled:function(){this.enabled=!this.enabled},toggle:function(b){var c=b?a(b.currentTarget)[this.type](this._options).data(this.type):this;c.tip().hasClass("in")?c.hide():c.show()},destroy:function(){this.hide().$element.off("."+this.type).removeData(this.type)}};var c=a.fn.tooltip;a.fn.tooltip=function(c){return this.each(function(){var d=a(this),e=d.data("tooltip"),f=typeof c=="object"&&c;e||d.data("tooltip",e=new b(this,f)),typeof c=="string"&&e[c]()})},a.fn.tooltip.Constructor=b,a.fn.tooltip.defaults={animation:!0,placement:"top",selector:!1,template:'<div class="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>',trigger:"hover focus",title:"",delay:0,html:!1,container:!1},a.fn.tooltip.noConflict=function(){return a.fn.tooltip=c,this}}(window.jQuery);
 // CFE JavaScript
 ;(function ($) {
-		
+    var root = "http://www.cfe.umich.edu/sandbox/";
+
+    function getParameterByName(name) {
+        var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+        return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+    }
+
 	$(document).ready(function() {
-		/* This seems to be incompatible with the old version of jQuery.
-		//$('.social-icons').tooltip({
-		//	'placement': 'bottom'
-		});
-		*/
-		
 		var browserName  = navigator.appName;
 		var verOffsetMSIE= navigator.userAgent.indexOf("MSIE");
 		var fullVerMSIE = navigator.userAgent.substring(verOffsetMSIE+5);
@@ -40,8 +40,16 @@
 
 		}
 		
-		//$('body').append(browserName + '<br>' + verMSIE);
-		
+		// Small eye candies for the navigation menu
+        $('#navigation .menu .expanded').not('.menu .expanded .expanded').each(function(){
+            $(this).append("<div class='in'><div class='insideIn'></div></div>");
+        });
+        $('#navigation .menu .expanded').not('.menu .expanded .expanded').hover(function(){
+           $('.in',$(this)).show();
+        },function(){
+            $('.in',$(this)).hide();
+        });
+
 		/* Navigation breakage fix IE7: IT WORKS, BUT NOT TO THE FULLY SATISFACTORY EXTENT... SHOULD BE WORKED ON LATER */
 		if(verOffsetMSIE != -1 && (verMSIE == '7.0')){
 			$('#navigation ul li').hover(function(){
@@ -222,36 +230,77 @@
 		if($('body').hasClass('page-frontpage')){
 			var videoContent = "<div class='sidebarMargin'>"+$('.view-latest-event .attachment-after .view-display-id-attachment_2').html()+"</div>";
 			$('.view-latest-event .attachment-after .view-display-id-attachment_2').empty();
-			$('#page_contents .region-sidebar-first .#block-block-2 .content div[class=tweets-pulled-listing]').after("<br>"+videoContent);
+			$('#page_contents .region-sidebar-first #block-block-2 .content div[class=tweets-pulled-listing]').after("<br>"+videoContent);
 		}
 		/* -- Sidebar styling ends here -- */
 		
 		/* Match the links of the thumbnail images on News and Events pages with those of the titles */
-		if($('body').hasClass('page-press') || $('body').hasClass('page-events')){
+		if($('body').hasClass('page-press') || $('body').hasClass('page-events') || $('body').hasClass('page-careers')){
 			$('.views-field-field-image a').each(function(){
 				$(this).attr('href', $(this).parent().parent().parent().find('.views-field-field-url a').attr('href'));
 			});
 		}
 		
 		/* Remove the 'button' classes from the titles on the News page and Events page */
-		if($('body').hasClass('page-press') || $('body').hasClass('page-events')){
+		if($('body').hasClass('page-press') || $('body').hasClass('page-events') || $('body').hasClass('page-careers')){
 			$('a[class=button]','body').not('#scrollToTop a').removeClass('button');
 		}
+
+        /* -- Archive view layout in News page / Events page starts here -- */
+        if($('body').attr('class').indexOf('page-press-') != -1 || $('body').attr('class').indexOf('page-events-') != -1){
+            var loc = location.href.split('/');
+            $('hr','div[class=view-footer]').remove();
+            var year = parseInt((loc[loc.length-1].substr(0,loc[loc.length-1].indexOf('-'))));
+            var month = parseInt(loc[loc.length-1].substr(loc[loc.length-1].indexOf('-')+1));
+            var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+            //$('.wrapper #page_contents .col_2 .content .view-display-id-attachment_1','body').empty();
+            $('.wrapper #page_contents .col_2 .content .view-display-id-attachment_1','body').prepend("<h2>"+monthNames[month-1]+" "+year+"</h2>");
+
+        }
+        /* -- Archive view layout in News page ends here -- */
+
+        /* Enable directly going into the editing mode upon double-clicking on either the title or image of an item in Press and Event pages */
+        if($('body').hasClass('page-press') || $('body').hasClass('page-events') || $('body').hasClass('page-careers')){
+            $('.views-field-field-image').each(function(){
+                $(this).after('<div class="editShortcutContainer"><div class="editShortcut"><img src="http://www.cfe.umich.edu/img/glyphicons_030_pencil.png"></div></div>');
+            });
+
+            $('.views-field-field-url').filter(function(index){
+                return ($(this).parent().find('.views-field-field-image').html() == null);
+            }).after('<div class="editShortcutContainer" style="top:81px; left:0;"><div class="editShortcut"><img src="http://www.cfe.umich.edu/img/glyphicons_030_pencil.png"></div></div>');
+
+            $('img','.editShortcut').click(function(){
+                var curLocation = location.href.split('/');
+                if($('.view-id-news .view-display-id-attachment_1 h2, .view-id-events .view-display-id-attachment_1 h2').html() == null){
+                    location.href=$('a',$(this).parent().parent().parent().find('.views-field-view-node')).attr('href')+'?origin='+curLocation[curLocation.length - 1]+'#overlay=node/'+$('.views-field-nid .nodeId', $(this).parent().parent().parent()).text()+'/edit';
+                }else{
+                    location.href=$('a',$(this).parent().parent().parent().find('.views-field-view-node')).attr('href')+'?origin='+curLocation[curLocation.length - 2]+'&archive='+curLocation[curLocation.length - 1]+'#overlay=node/'+$('.views-field-nid .nodeId', $(this).parent().parent().parent()).text()+'/edit';
+                }
+
+            });
+            $('.views-field-field-url a, .views-field-field-image a').each(function(){
+                var curLocation = location.href.split('/');
+
+                if($('.view-id-news .view-display-id-attachment_1 h2, .view-id-events .view-display-id-attachment_1 h2').html() == null){
+                    $(this).attr('href', $(this).attr('href')+'?origin='+curLocation[curLocation.length - 1]);
+                }else{
+
+                    $(this).attr('href', $(this).attr('href')+'?origin='+curLocation[curLocation.length - 2]+'&archive='+curLocation[curLocation.length - 1]);
+                }
+            })
+
+        }
+
+        /* -- Back-end stuff for Back Navigation in People pages -- */
+        if($('body').hasClass('page-staff') || $('body').hasClass('page-interns') || $('body').hasClass('page-board') || $('body').hasClass('page-faculty-affiliates')){
+            var curLocation = location.href.split('/');
+            $('.views-field-field-image a, .views-field-title .imgHeader a').each(function(){
+                $(this).attr('href', $(this).attr('href')+'?origin='+curLocation[curLocation.length -1]);
+            });
+        }
 			
-		/* -- Archive view layout in News page / Events page starts here -- */
-		if($('body').attr('class').indexOf('page-press-') != -1 || $('body').attr('class').indexOf('page-events-') != -1){
-			var loc = location.href.split('/');
-			$('hr','div[class=view-footer]').remove();
-			var year = parseInt((loc[loc.length-1].substr(0,loc[loc.length-1].indexOf('-'))));
-			var month = parseInt(loc[loc.length-1].substr(loc[loc.length-1].indexOf('-')+1));
-			var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-			
-			//$('.wrapper #page_contents .col_2 .content .view-display-id-attachment_1','body').empty();
-			$('.wrapper #page_contents .col_2 .content .view-display-id-attachment_1','body').prepend("<h2>"+monthNames[month-1]+" "+year+"</h2>");
-			
-		}
-		/* -- Archive view layout in News page ends here -- */
-		
+
 		/* -- Article(External) view styling starts here -- */
 		if($('body').hasClass('node-type-article-external-')){
 			// Make a little bit of space between the published date and the link button
@@ -520,6 +569,58 @@
         /* -- Blog sub-header conditional statement -- */
         if($('body').hasClass('page-blog') && $('body').attr('class').indexOf('page-blog-') < 0){
             $('.col_2 .content .view-header h2').text('Posts by');
+        }
+
+        /* -- Back button for multi-layered pages -- */
+        if($('body').hasClass('node-type-person') || $('body').hasClass('node-type-article') || $('body').hasClass('node-type-article-external-') || $('body').hasClass('node-type-entr407-recordings') || $('body').hasClass('node-type-courses')){
+            var originURL = getParameterByName('origin');
+            $('h2[id=page-title]').after('&nbsp;&nbsp;<a class="backBtn">Back to List</a>');
+
+            if(document.referrer.indexOf('#') == -1){
+                var str = document.referrer.split('/');
+
+
+                if(getParameterByName('origin') != null){
+                    if(getParameterByName('archive') != null){
+                        $('.backBtn').attr('href', root + getParameterByName('origin') + '/' + getParameterByName('archive'));
+                    }else{
+                        $('.backBtn').attr('href', root + getParameterByName('origin'));
+                    }
+
+                }else{
+                    var str = document.referrer.split('/');
+                    var idxOfOrigin = str[str.length-1].indexOf('?origin=');
+                    var idxOfArchive = str[str.length-1].indexOf('&archive=');
+                    if(idxOfArchive != -1){
+                        $('.backBtn').attr('href', root + str[str.length-1].substr(idxOfOrigin+8, idxOfArchive - idxOfOrigin - 8) + '/' + str[str.length-1].substr(idxOfArchive+9));
+
+                    }else{
+                        $('.backBtn').attr('href', root + str[str.length-1].substr(idxOfOrigin+8));
+                    }
+                }
+
+                //$('.backBtn').attr('href', root + document.referrer);
+            }
+            /*
+            else{ // when an article has been edited
+                var str = document.referrer.split('/');
+                var idx_originStr = str[str.length - 1].indexOf('origin=');
+                var idx_archiveStr = str[str.length - 1].indexOf('archive=');
+
+                if(idx_archiveStr == -1){
+                    alert(str[str.length-1].substr(idx_originStr+7));
+                }else{
+
+                }
+            }
+            */
+        }
+
+        /* -- Back button for pages that have an archive section -- */
+        if($('body').attr('class').indexOf('page-press-') || $('body').attr('class').indexOf('page-events-')){
+            var curLocation = location.href.split('/');
+            $('.view-id-news .view-display-id-attachment_1 h2, .view-id-events .view-display-id-attachment_1 h2').after('&nbsp;&nbsp;<a class="backBtn2">Back to List</a>');
+            $('.backBtn2').attr('href', root + curLocation[curLocation.length - 2]);
         }
 	
 	}); // ready
