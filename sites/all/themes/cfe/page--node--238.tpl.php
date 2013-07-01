cebc34506c71c343e798cdeec153e7668126e2cd
jooskim <jsk9260@gmail.com>
2013-07-01 13:55:44 -0400

    New footer

--- sites/all/themes/cfe/page--node--238.tpl.php
+++ sites/all/themes/cfe/page--node--238.tpl.php
@@ -39,13 +39,20 @@
 
 		<!-- Footer: Start -->
 		<footer>
-			<div class="footer_col">
+            <!--<div class="footer_col">
 				<a href="<?php print $base_path; ?>"><img src="<?php print $base_path . $directory; ?>/css/components/logo-cfe-mono.png" width="96" height="35" border="0"></a>
-			</div>
-			<div class="footer_col copyright"><a href="http://www.regents.umich.edu/">&copy; <?php echo date('Y'); ?>, The Regents of the University of Michigan</a> &nbsp; | &nbsp; <a href="<?php print $base_path . 'contact-us';?>">Contact Us</a><!-- &nbsp; | &nbsp; <a href="<?php print $base_path . 'faq';?>">Frequently Asked Questions</a>--></div>
-			<div class="footer_col">
+			</div>-->
+            <!--<div class="footer_col copyright"><a href="http://www.regents.umich.edu/">&copy; <?php echo date('Y'); ?>, The Regents of the University of Michigan</a> &nbsp; | &nbsp; <a href="<?php print $base_path . 'contact-us';?>">Contact Us</a>--><!-- &nbsp; | &nbsp; <a href="<?php print $base_path . 'faq';?>">Frequently Asked Questions</a></div>-->
+            <!--<div class="footer_col">
 				<img src="<?php print $base_path . $directory; ?>/css/components/logo-cfe-spark.png" width="30" border="0">
 			</div>
+			-->
+            <div class="footer_hr">
+                <div class="footer_logo">
+                    <a href="<?php print $base_path; ?>"><img src="<?php print $base_path; ?>img/cfe_footer_logo.png" border="0"></a>
+                </div>
+            </div>
+            <div class="footer_content">&copy; <?php echo date('Y'); ?>, Center for Entrepreneurship. All Rights Reserved.<br><a href="http://www.regents.umich.edu/">The Regents of the University of Michigan</a> &nbsp; | &nbsp; <a href="<?php print $base_path . 'contact-us';?>">Contact Us</a><br><br><span style="font-size:0.9em">Center for Entrepreneurship is best viewed in the following web browsers:</span><br><br><img src="<?php print $base_path; ?>img/browser_compatibility.png" border=0><!-- &nbsp; | &nbsp; <a href="<?php print $base_path . 'faq';?>">Frequently Asked Questions</a>--></div>
 		</footer>
 		<!-- Footer: End -->
 	</div>
